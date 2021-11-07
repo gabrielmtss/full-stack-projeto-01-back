d999d4a1a5cbf0b4ee2421ac8c0d5bf71234e135
Gabriel Matos <g.matos@outlook.com>
2021-11-07 16:21:34 -0300

    adiciona id aos objetos e endpoints

--- routes/jogos.routes.js
+++ routes/jogos.routes.js
@@ -3,20 +3,23 @@ const router = express.Router();
 
 let listaJogos = [
   {
+    id: Date.now(),
     titulo: "The Witcher 3: Wild Hunt",
     imagem: "https://upload.wikimedia.org/wikipedia/pt/0/06/TW3_Wild_Hunt.png",
     genero: "RPG",
     nota: 10,
     jogado: true
   },
-  {
+  { 
+    id: Date.now(),
     titulo: "Fortnite",
     imagem: "https://image.api.playstation.com/vulcan/img/rnd/202109/1303/kaXldqXxbIyvX9Kn8eeFckQJ.png",
     genero: "Battle royale",
     nota: 8,
     jogado: true
   },
-  {
+  { 
+    id: Date.now(),
     titulo: "Red Dead Redemption 2",
     imagem: "https://image.api.playstation.com/cdn/UP1004/CUSA03041_00/Hpl5MtwQgOVF9vJqlfui6SDB5Jl4oBSq.png",
     genero: "Ação",
@@ -36,6 +39,7 @@ router.get("/:id", (req, res) => {
 
 router.post("/add", (req, res) => {
   const jogo = req.body;
+  jogo.id = Date.now();
   listaJogos.push(jogo);
   res.status(201).send({
     message: `Jogo ${jogo.titulo} cadastrado com sucesso!`,
@@ -45,26 +49,32 @@ router.post("/add", (req, res) => {
 
 router.put("/edit/:id", (req, res) => {
   const jogoEdit = req.body;
-  const id = req.params.id -1 ;
+  const idParam = req.params.id;
   
-  listaJogos[id] = {
-    ...listaJogos[id],
+  let index = listaJogos.findIndex(jogo => jogo.id == idParam);
+
+  listaJogos[index] = {
+    ...listaJogos[index],
     ...jogoEdit
   }
 
   res.send({
-    message: `O jogo ${listaJogos[id].titulo} foi atualizado com sucesso!`,
+    message: `O jogo ${listaJogos[index].titulo} foi atualizado com sucesso!`,
     data: listaJogos[id]
   });
 });
 
 router.delete("/delete/:id", (req, res) => {
-  const id = req.params.id -1 ;
+  const idParam = req.params.id;
   const jogo = listaJogos[id];
 
-  listaJogos.splice(id, 1);
+  const index = listaJogos.findIndex(jogo => jogo.id == idParam);
+
+  const nome = listaJogos[index];
+
+  listaJogos.splice(index, 1);
   res.send({
-    message: `O jogo ${jogo.titulo} foi excluido com sucesso! `
+    message: `O jogo ${nome.titulo} foi excluido com sucesso! `
   });
 });
 
